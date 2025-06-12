c18a108e3b09781468826c04635dc3ff144a202c
Masud Parvez <thisismasud1@gmail.com>
2025-06-12 19:31:45 +0600

    fixes a theme issue

--- src/components/CartDetails.jsx
+++ src/components/CartDetails.jsx
@@ -4,7 +4,7 @@ import { getImageUrl } from "../utils/getImageUtility";
 
 export default function CartDetails({ onClose, cartData, dispatch }) {
   return (
-    <div className="fixed top-0 left-0 w-screen h-screen z-50 bg-black/60 backdrop-blur-sm">
+    <div className="fixed top-0 left-0 w-screen h-screen z-50 bg-black/60 backdrop-blur-sm text-white">
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[420px] sm:max-w-[600px] lg:max-w-[790px] p-4 max-h-[90vh] overflow-auto">
         <div className="bg-white shadow-md dark:bg-[#12141D] rounded-2xl overflow-hidden p-5 md:p-9">
           <h2 className="text-2xl lg:text-[30px] mb-10 font-bold">
--- src/components/MovieModal.jsx
+++ src/components/MovieModal.jsx
@@ -2,7 +2,7 @@ import { getImageUrl } from "../utils/getImageUtility";
 import Rating from "./MovieList/Rating";
 export default function MovieModal({ movie, onClose, addToCart }) {
   return (
-    <div className="fixed top-0 left-0 w-screen h-screen z-50 bg-black/60 backdrop-blur-sm">
+    <div className="fixed top-0 left-0 w-screen h-screen z-50 bg-black/60 backdrop-blur-sm text-white">
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[420px] sm:max-w-[600px] lg:max-w-[984px] p-4 max-h-[90vh] overflow-auto">
         <div className="bg-white shadow-md dark:bg-[#12141D] rounded-2xl sm:grid sm:grid-cols-[2fr_1fr] overflow-hidden">
           <img
